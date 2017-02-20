0f3273674bae90f73cfd8e868c4426e558c3cbfd
Chris M <robotpuppetmaster@gmail.com>
2017-02-20 09:34:40 -0500

    Made some changes to server, created form

--- server.py
+++ server.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/movie')
+def movie():
+    return render_template('movie.html')
+
+@app.route('/addmovie', methods = {'POST'})
+def addmovie():
+    return render_template('home.html')
